2fa3d35f90a4b3a86d831c9ced167ef6216cd5b5
lexi <tori@disroot.org>
2023-10-03 23:01:53 +0200

    oopsie
    Signed-off-by: lexi <tori@disroot.org>

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,11 +1,2 @@
-import { getI18nStores } from '../i18n/i18n.js';
-import type { LayoutLoad } from './$types';
-
-export const load: LayoutLoad = async ({ data }) => {
-    return {
-        ...(await getI18nStores(data.requestLocale))
-    };
-};
-
 export const prerender = true;
 
